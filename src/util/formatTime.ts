ea8ba49dfb96201747b7688931b675e384a4aa2e
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2020-03-23 22:23:12 +1030

    Format the time in the log nicer

--- src/util/formatTime.ts
+++ src/util/formatTime.ts
@@ -1,9 +1,14 @@
+function pad(numb: number, length: number) {
+	return numb.toString().padStart(length, '0');
+}
+
 export function formatTime(date: Date): string {
-	const year = date.getFullYear();
-	const month = date.getMonth();
-	const day = date.getDate();
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
-	const seconds = date.getSeconds();
+	const year = pad(date.getFullYear(), 4);
+	const month = pad(date.getMonth(), 2);
+	const day = pad(date.getDate(), 2);
+	const hours = pad(date.getHours(), 2);
+	const minutes = pad(date.getMinutes(), 2);
+	const seconds = pad(date.getSeconds(), 2);
+
 	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
---
+++ test/util/formatTime.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import 'mocha';
+import { formatTime } from '../../src/util/formatTime';
+
+describe('formatTime', () => {
+	it('Should format the time nicely', () => {
+		const result = formatTime(new Date(2020, 3, 23, 22, 20, 9));
+
+		expect(result).to.equal('2020-03-23 22:20:09');
+	});
+});
