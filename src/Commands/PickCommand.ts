18bc79ff92877530b9935ffe906e145c09ad5b3e
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2020-04-11 03:15:18 +0930

    Add some nicer responses to the pick command

--- src/Commands/PickCommand.ts
+++ src/Commands/PickCommand.ts
@@ -2,19 +2,23 @@ import Message from '../ChatServer/Message';
 import Command from '../Command';
 import Tracery from '../tracery/Tracery';
 import selectRandom from '../util/selectRandom';
+import sleep from '../util/sleep';
 
 const grammar = {
 	helpText: ["Give me some options and I'll pick one randomly."],
 	example: ['!pick "Rocket League" Overwatch Factorio Minecraft'],
 	answer: [
-		'Hmm, I choose "#choice#.',
-		'"#choice#"! Definitely.',
+		'Hmm,<sleep>I choose "#choice#".',
+		'"#choice#"!\nDefinitely.',
 		'I really think the answer is "#choice#".',
+		'James told me it was "#choice#".',
+		'Today\'s fortune cookie says "#choice#".',
 	],
 
 	answer1: [
-		'I\'m really not sure, but I think "#choice#"',
+		'I\'m really not sure, but I think "#choice#".',
 		'Really? "#choice#" it is then...',
+		'The law requires that I answer "#choice#".',
 	],
 };
 
@@ -38,14 +42,20 @@ export default class PickCommand implements Command {
 
 		const pick = selectRandom(args);
 
-		await channel.sendText(
-			Tracery.generate(
-				{
-					...grammar,
-					choice: Tracery.escape(pick),
-				},
-				args.length === 1 ? 'answer1' : 'answer'
-			)
-		);
+		const text = Tracery.generate(
+			{
+				...grammar,
+				choice: Tracery.escape(pick),
+			},
+			args.length === 1 ? 'answer1' : 'answer'
+			//TODO: Move handling of <sleep> to some kind of rich text handler
+		).split(/<sleep>/g);
+
+		for (let i = 0; i < text.length; i++) {
+			if (i > 0) {
+				await sleep(800);
+			}
+			await channel.sendText(text[i]);
+		}
 	}
 }
