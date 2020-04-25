9d26e18518e324be436a9123404c899eddbfc4d1
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2020-04-25 16:34:32 +0930

    Attach @dev to version when built in dev mode

--- scripts/injectVersion.js
+++ scripts/injectVersion.js
@@ -8,10 +8,32 @@ const version = package.version;
 const location = path.join(__dirname, '..', 'dist');
 const filename = path.join(location, 'version');
 
+const args = process.argv;
+args.shift(); // node.exe
+args.shift(); // injectVersion.js
+
+let mode;
+if (args.length > 0) {
+	switch (args[0]) {
+		case '--dev':
+		case '--development':
+			mode = 'development';
+			break;
+
+		case '--prod':
+		case '--production':
+		default:
+			mode = 'production';
+			break;
+	}
+}
+
 if (!fs.existsSync(location)) {
 	console.warn('Created dist directory for version injection');
 	fs.mkdirSync(location, { recursive: true });
 }
 
-fs.writeFileSync(filename, `v${version}`);
-console.log(`Version ${version} injected at ${path.resolve(location)}`);
+const modeStr = mode === 'development' ? '@dev' : '';
+const versionStr = `v${version}${modeStr}`;
+fs.writeFileSync(filename, versionStr);
+console.log(`Version ${versionStr} injected at ${path.resolve(location)}`);
