d17af3e66dec4272c0245206164f65648503b92f
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2022-05-13 20:45:55 +0930

    fix: add permissions to read server messages (and update api)

--- src/Plugins/Discord/ChatServer/DiscordServerFactory.ts
+++ src/Plugins/Discord/ChatServer/DiscordServerFactory.ts
@@ -16,7 +16,28 @@ export default class DiscordServerFactory {
 		return new DiscordServer({
 			id: serverConfig.id ?? 'Discord',
 			discordClient: new Client({
-				intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES],
+				intents: [
+					// Both GUILDS and GUILD_MESSAGES are required to get messages in server channels.
+					// these permissions are a pain so i'm leaving them all in except these two, see here
+					// https://discord.com/developers/docs/topics/gateway#privileged-intents
+
+					Intents.FLAGS.GUILDS,
+					// Intents.FLAGS.GUILD_MEMBERS,
+					Intents.FLAGS.GUILD_BANS,
+					Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+					Intents.FLAGS.GUILD_INTEGRATIONS,
+					Intents.FLAGS.GUILD_WEBHOOKS,
+					Intents.FLAGS.GUILD_INVITES,
+					Intents.FLAGS.GUILD_VOICE_STATES,
+					// Intents.FLAGS.GUILD_PRESENCES,
+					Intents.FLAGS.GUILD_MESSAGES,
+					Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+					Intents.FLAGS.GUILD_MESSAGE_TYPING,
+					Intents.FLAGS.DIRECT_MESSAGES,
+					Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+					Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+					Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
+				],
 				partials: ['CHANNEL'],
 			}),
 			botToken: serverConfig.botToken,
